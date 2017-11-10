a0d5e3b4248bba6865bb3b0cedb91b55d8ae3f5e
jackhaydock <jwhaydock@gmail.com>
2017-11-10 17:01:03 +0000

    Pycat Deletion Fix 10/11/17

--- templates/delfac.sql
+++ templates/delfac.sql
@@ -1,8 +1,10 @@
 use icat;
 
-delete from DATAFILEFORMAT where DESCRIPTION RLIKE "topcat";
+select ID from FACILITY WHERE NAME = "LILS";
 
-delete from FACILITY where NAME = "{{ fac_name }}";
+#delete from DATAFILEFORMAT where DESCRIPTION RLIKE "topcat";
+
+#delete from FACILITY where NAME = "{{ fac_name }}";
 
 delete from USER_ where NAME RLIKE "^db/user";
 delete from USER_ where NAME = "{{ icat_mech }}/{{ icat_root }}";
